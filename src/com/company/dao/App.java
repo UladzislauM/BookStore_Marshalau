dda9ded0457e72869923119095f816bd194ab9f6
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-08-04 13:26:47 +0300

    Change App - Delete throws Exeption.

--- src/com/company/dao/App.java
+++ src/com/company/dao/App.java
@@ -5,7 +5,7 @@ import com.company.dao.controller.ControllerBook;
 public class App {
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ControllerBook controllerBook = new ControllerBook();
         controllerBook.consoleInterface();
     }
